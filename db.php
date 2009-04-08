4c5e8932ea1145fb4febd61ed626b12c427d6d8e
eugeniy <burump@gmail.com>
2009-04-08 14:14:53 -0500

    stubs for database classes

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * Database Abstract Adapter
+ *
+ * @package    Scaffold
+ * @author     Eugeniy Kalinin
+ * @copyright  Copyright (c) 2009, Eugeniy Kalinin
+ * @license    http://pandabytes.info/license
+ *
+ */
+abstract class Scaffold_Db
+{
+
+}
---
+++ db/mysql.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * Mysql Database Adapter
+ *
+ * @package    Scaffold_Db
+ * @author     Eugeniy Kalinin
+ * @copyright  Copyright (c) 2009, Eugeniy Kalinin
+ * @license    http://pandabytes.info/license
+ *
+ */
+class Scaffold_Db_Mysql extends Scaffold_Db
+{
+
+}
